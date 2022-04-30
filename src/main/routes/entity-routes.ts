ddc2d13362c5fa533dd239400118732703ab3ce7
TonySandro <tonysduarte101@gmail.com>
2022-04-30 10:54:24 -0300

    refactor: create single file from routes

--- src/main/routes/entity-routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { adaptRoute } from "../../main/adapters/express/express-routes-adapter";
-import { makeGetAllEntityController } from "../../main/factories/entity/entity-factory";
-
-export default (router: Router): void => {
-    router.get('/entities', adaptRoute(makeGetAllEntityController()))
-}
---
+++ src/main/routes/get-all-entity/entity-routes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { adaptRoute } from "../../../main/adapters/express/express-routes-adapter";
+import { makeGetAllEntityController } from "../../../main/factories/entity/entity-factory";
+
+export default (router: Router): void => {
+    router.get('/entities', adaptRoute(makeGetAllEntityController()))
+}
