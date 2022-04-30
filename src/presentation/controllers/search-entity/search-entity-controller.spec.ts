58409f05f2e90fca090e1865a5075563d77d8ea5
TonySandro <tonysduarte101@gmail.com>
2022-04-29 21:57:28 -0300

    test: ensure SearchEntityController return 500 if readEntity throws

--- src/presentation/controllers/search-entity/search-entity-controller.spec.ts
+++ src/presentation/controllers/search-entity/search-entity-controller.spec.ts
@@ -1,6 +1,17 @@
-import { success } from "../../helpers/http/http-helper"
+import { serverError, success } from "../../helpers/http/http-helper"
 import { EntityModel } from "../../../domain/models/entity"
 import { SearchEntityController } from "./search-entity-controller"
+import { ReadEntity } from "../../../domain/usecases/get-all-entity"
+import { ServerError } from "../../errors"
+
+const makeReadEntity = (): ReadEntity => {
+    class ReadEntityStub implements ReadEntity {
+        async getAll(): Promise<EntityModel[]> {
+            return new Promise(resolve => resolve(makeFakeResponse()))
+        }
+    }
+    return new ReadEntityStub()
+}
 
 const makeFakeRequest = () => ({
     body: 'any_entity'
@@ -28,9 +39,11 @@ const makeFakeResponse = (): EntityModel[] => ([{
 }])
 
 const makeSut = () => {
-    const sut = new SearchEntityController()
+    const readEntity = makeReadEntity()
+    const sut = new SearchEntityController(readEntity)
     return {
-        sut
+        sut,
+        readEntity
     }
 }
 
@@ -40,5 +53,14 @@ describe('Search Entity Controller', () => {
 
         const httpResponse = await sut.handle(makeFakeRequest())
         expect(httpResponse).toEqual(success([]))
+    })
+
+    test('Should return 500 if readEntity throws', async () => {
+        const { sut, readEntity } = makeSut()
+        jest.spyOn(readEntity, 'getAll').mockImplementationOnce(async () => {
+            return new Promise((resolve, reject) => reject(new Error()))
+        })
+        const httpResponse = await sut.handle(makeFakeRequest())
+        expect(httpResponse).toEqual(serverError(new ServerError(null)))
     })
 })
