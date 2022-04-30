cf56dd65a1c098b44520db5fe59c66758bfa53b7
TonySandro <tonysduarte101@gmail.com>
2022-04-29 21:57:17 -0300

    feat: ensure SearchEntityController return 500 if readEntity throws

--- src/presentation/controllers/search-entity/search-entity-controller.ts
+++ src/presentation/controllers/search-entity/search-entity-controller.ts
@@ -1,9 +1,18 @@
-import { success } from '../../../presentation/helpers/http/http-helper'
+import { ReadEntity } from '../../../domain/usecases/get-all-entity'
+import { serverError, success } from '../../../presentation/helpers/http/http-helper'
 import { HttpRequest, HttpResponse } from '../get-all-entity/entity-controller-protocols'
 import { Controller } from './search-entity-controller-protocols'
 
 export class SearchEntityController implements Controller {
+    constructor(private readonly readEntity: ReadEntity) { }
+
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        return success([])
+        try {
+            await this.readEntity.getAll()
+
+            return success([])
+        } catch (error) {
+            return serverError(error)
+        }
     }
 }
