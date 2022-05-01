1c5dbcf097a483b9302366cf800e8c1ac8b56a4a
TonySandro <tonysduarte101@gmail.com>
2022-04-30 22:25:50 -0300

    feat: ensure SearchEntityController return 400 if no entityName is provided

--- src/presentation/controllers/search-entity/search-entity-controller.ts
+++ src/presentation/controllers/search-entity/search-entity-controller.ts
@@ -1,5 +1,6 @@
+import { MissingParamError } from '../../../presentation/errors'
 import { ReadEntity } from '../../../domain/usecases/get-all-entity'
-import { serverError, success } from '../../../presentation/helpers/http/http-helper'
+import { badRequest, serverError, success } from '../../../presentation/helpers/http/http-helper'
 import { HttpRequest, HttpResponse } from '../get-all-entity/entity-controller-protocols'
 import { Controller } from './search-entity-controller-protocols'
 
@@ -8,6 +9,12 @@ export class SearchEntityController implements Controller {
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
+            const { entityName } = httpRequest.body
+
+            if (!entityName) {
+                return badRequest(new MissingParamError("entityName"))
+            }
+
             await this.readEntity.getAll()
 
             return success([])
