312d39a24380c8308b4c194dbfdb3a600afdf3d9
TonySandro <tonysduarte101@gmail.com>
2022-05-01 11:33:12 -0300

    refactor: rename DbReadEntity from DbReadAllEntity

--- src/data/usecases/read-all-entity/db-read-all-entity.ts
+++ src/data/usecases/read-all-entity/db-read-all-entity.ts
@@ -2,7 +2,7 @@ import { ReadEntityRepository } from "data/protocols/db/entity/read-entity-repos
 import { EntityModel } from "domain/models/entity"
 import { ReadEntity } from "domain/usecases/get-all-entity"
 
-export class DbReadEntity implements ReadEntity {
+export class DbReadAllEntity implements ReadEntity {
     constructor(private readonly readEntity: ReadEntityRepository) { }
     async getAll(): Promise<EntityModel[]> {
         return await this.readEntity.getAll()
--- src/main/factories/entity/entity-factory.ts
+++ src/main/factories/entity/entity-factory.ts
@@ -1,9 +1,9 @@
 import { ReadAllEntityMongoRepository } from "../../../infra/db/mongodb/entity/readAll/read-all-entity-mongo-repository";
-import { DbReadEntity } from "../../../data/usecases/read-entity/db-read-entity";
+import { DbReadAllEntity } from "../../../data/usecases/read-all-entity/db-read-all-entity";
 import { GetAllEntityController } from "../../../presentation/controllers/get-all-entity/entity-controller";
 
 export const makeGetAllEntityController = (): GetAllEntityController => {
     const dbEntituMongoRepository = new ReadAllEntityMongoRepository()
-    const dbReadEntity = new DbReadEntity(dbEntituMongoRepository)
-    return new GetAllEntityController(dbReadEntity)
+    const dbReadAllEntity = new DbReadAllEntity(dbEntituMongoRepository)
+    return new GetAllEntityController(dbReadAllEntity)
 }
