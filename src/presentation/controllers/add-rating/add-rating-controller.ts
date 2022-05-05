23dc5abed94a5a9d97b26f3637c4c5868b619985
TonySandro <tonysduarte101@gmail.com>
2022-05-05 12:23:38 -0300

    feat: ensure AddRatingController return 400 if goBack/satisfaction is  not provided

--- src/presentation/controllers/add-rating/add-rating-controller.ts
+++ src/presentation/controllers/add-rating/add-rating-controller.ts
@@ -5,7 +5,7 @@ import { Controller, HttpRequest, HttpResponse } from "../controller-protocols";
 export class AddRatingController implements Controller {
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         const { assessments } = httpRequest.body
-        const requiredField = ["indicate"]
+        const requiredField = ["indicate", "goBack", "satisfaction"]
 
         for (const field of requiredField) {
             if (!assessments[field]) {
