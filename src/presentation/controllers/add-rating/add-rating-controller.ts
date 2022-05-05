d110e36a6ea61831f9143e92ed3997067b0266b8
TonySandro <tonysduarte101@gmail.com>
2022-05-05 12:20:52 -0300

    feat: ensure AddRatingController return 400 if no indicate is provided

--- src/presentation/controllers/add-rating/add-rating-controller.ts
+++ src/presentation/controllers/add-rating/add-rating-controller.ts
@@ -1,8 +1,17 @@
-import { success } from "../../helpers/http/http-helper";
+import { MissingParamError } from "../../errors";
+import { badRequest, success } from "../../helpers/http/http-helper";
 import { Controller, HttpRequest, HttpResponse } from "../controller-protocols";
 
 export class AddRatingController implements Controller {
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        const { assessments } = httpRequest.body
+        const requiredField = ["indicate"]
+
+        for (const field of requiredField) {
+            if (!assessments[field]) {
+                return badRequest(new MissingParamError(field))
+            }
+        }
 
         return success(null)
     }
