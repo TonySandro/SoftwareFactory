ef6bf42d1e872b36387b86cf80dba787876ea4a1
Tony Sandro <tonysduarte101@gmail.com>
2022-05-14 00:03:13 -0300

    refactor: correction  controller-protocols EmailValidator

--- src/presentation/helpers/validators/email-validation.spec.ts
+++ src/presentation/helpers/validators/email-validation.spec.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError } from "../../errors"
-import { EmailValidator } from "../../controllers/signup/signup-controller-protocols"
+import { EmailValidator } from "../../controllers/controller-protocols"
 import { EmailValidation } from "./email-validation"
 
 const makeEmailValidator = (): EmailValidator => {
