c29345cfb23d3de0db5e89011bb78c99c90809a6
Tony Sandro <tonysduarte101@gmail.com>
2022-05-22 19:01:53 -0300

    feat: adding CommentaryModel

--- src/domain/models/rating.ts
+++ src/domain/models/rating.ts
@@ -1,7 +1,13 @@
+export interface CommentaryModel {
+    autor: string
+    message: string
+    star: number
+}
+
 export interface RatingModel {
     entityName: string
     indicate: number
     goBack: number
     satisfaction: number
-    commentary?: string
+    commentary?: CommentaryModel
 }
